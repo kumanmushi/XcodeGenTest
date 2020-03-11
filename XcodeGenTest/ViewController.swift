4d4c55f8a4fadc4fe2d097e4343aff14338b62ed
村田真矢 <>
2020-03-11 11:51:43 +0900

    プロジェクトを追加

---
+++ XcodeGenTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenTest
+//
+//  Created by 村田真矢 on 2020/03/11.
+//  Copyright © 2020 村田真矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
