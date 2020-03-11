ad3010bd1566837ca70b6c8e5e542c13344bea17
村田真矢 <>
2020-03-11 13:22:58 +0900

    CocoaPodsの依存を追加

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'XcodeGenTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'NVActivityIndicatorView','4.7.0'
+
+  target 'XcodeGenTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'XcodeGenTestUITests' do
+    # Pods for testing
+  end
+
+end
--- XcodeGenTest/ViewController.swift
+++ XcodeGenTest/ViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 import Kingfisher
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
+    
+    let activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(activityIndicatorView)
+        activityIndicatorView.startAnimating()
+        
         imageView.kf.setImage(with: URL(string: "https://picsum.photos/350/250/?random"))
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        activityIndicatorView.center = view.center
+    }
 
 }
 
